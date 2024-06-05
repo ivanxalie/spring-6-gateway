9cd0b9565d949775587253462678c925899b57fa
ivanxalie <ivanxalie@gmail.com>
2024-06-05 20:02:48 +0300

    Fix security config and add logbook

--- src/main/java/guru/springframework/spring6gateway/config/SecurityConfig.java
+++ src/main/java/guru/springframework/spring6gateway/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package guru.springframework.spring6gateway.config;
 import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
@@ -16,21 +15,12 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    @Order(2)
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(CsrfSpec::disable)
+                .authorizeExchange(spec -> spec.matchers(EndpointRequest.toAnyEndpoint()).permitAll())
                 .authorizeExchange(spec -> spec.anyExchange().authenticated())
                 .oauth2ResourceServer(spec -> spec.jwt(withDefaults()))
-                .build();
-    }
-
-    @Bean
-    @Order(1)
-    public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity security) throws Exception {
-        return security
-                .authorizeExchange(matcherRegistry -> matcherRegistry.matchers(EndpointRequest.toAnyEndpoint())
-                        .permitAll())
                 .build();
     }
 }
